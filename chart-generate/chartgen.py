e250089ed8f172d6b4428a56674b808ebd9ba29c
Jae Bogg <26292620+JaeTLDR@users.noreply.github.com>
2024-09-14 22:50:53 +1000

    fix typos

--- chart-generate/chartgen.py
+++ chart-generate/chartgen.py
@@ -19,7 +19,7 @@ doc = json.loads(g.serialize(format='json-ld'))
 # print(doc)
 framed = jsonld.frame(doc, frame)
 
-with open('./chart.json', 'w',encoding = 'utf8') as out:
+with open('./out/chart.json', 'w',encoding = 'utf8') as out:
     out.write(json.dumps(framed,ensure_ascii=False,indent=4).encode('utf8').decode())
 
 #save chart segments 
@@ -33,14 +33,14 @@ def split_chart(root: dict, inc: list[str]):
 
 
 
-with open('./chart.1.json', 'w',encoding = 'utf8') as out:
+with open('./out/chart.1.json', 'w',encoding = 'utf8') as out:
     out.write(json.dumps(split_chart(framed["hasTopConcept"][0],['ischart:Cretaceous','ischart:Paleogene','ischart:Neogene','ischart:Quaternary']),ensure_ascii=False,indent=4).encode('utf8').decode())
 
-with open('./chart.2.json', 'w',encoding = 'utf8') as out:
+with open('./out/chart.2.json', 'w',encoding = 'utf8') as out:
     out.write(json.dumps(split_chart(framed["hasTopConcept"][0],['ischart:Jurassic','ischart:Triassic','ischart:Permian','ischart:Carboniferous']),ensure_ascii=False,indent=4).encode('utf8').decode())
 
-with open('./chart.3.json', 'w',encoding = 'utf8') as out:
+with open('./out/chart.3.json', 'w',encoding = 'utf8') as out:
     out.write(json.dumps(split_chart(framed["hasTopConcept"][0],['ischart:Devonian','ischart:Silurian','ischart:Ordovician','ischart:Cambrian']),ensure_ascii=False,indent=4).encode('utf8').decode())
 
-with open('./chart.4.json', 'w',encoding = 'utf8') as out:
+with open('./out/chart.4.json', 'w',encoding = 'utf8') as out:
     out.write(json.dumps(framed["hasTopConcept"][1],ensure_ascii=False,indent=4).encode('utf8').decode())
