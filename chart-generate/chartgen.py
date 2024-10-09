737fcbe96e93d6ab47d8fbf7b0c20fc35aaf2681
Jae Bogg <26292620+JaeTLDR@users.noreply.github.com>
2024-10-09 23:51:52 +1000

    add hierachy

--- chart-generate/chartgen.py
+++ chart-generate/chartgen.py
@@ -22,6 +22,10 @@ framed = jsonld.frame(doc, frame)
 with open('./out/chart.json', 'w',encoding = 'utf8') as out:
     out.write(json.dumps(framed,ensure_ascii=False,indent=4).encode('utf8').decode())
 
+
+
+
+
 #save chart segments 
 
 def split_chart(root: dict, inc: list[str]):
@@ -32,6 +36,37 @@ def split_chart(root: dict, inc: list[str]):
     return local
 
 
+def get_sub_info(node,root):
+    print(node["id"])
+    if 'broader' in node:
+        broader = node['broader']
+    else:
+        broader = None
+    if 'narrower' in node:
+        for n in node['narrower']:
+            root[n["id"]] =get_sub_info(n, root)
+        return {
+                'narrower': [n["id"] for n in node['narrower']],
+                'broader':broader# node['broader']
+            }
+    else:
+        return {
+                'narrower': None, #[n for n in node['narrower']],
+                'broader': broader #node['broader']
+            }
+    
+def get_hierachy():
+    root = {}
+    topConcepts = [tl for tl in framed["hasTopConcept"] ]
+    for tc in topConcepts:
+        root[tc["id"]] = get_sub_info(tc, root)
+    return root
+    #{
+    #   "ischart:someendpoint":{narrower:["ischart:xxx"],broader:["ischart:xxx"]}
+    #}
+    
+with open('./out/chart.hierachy.json', 'w',encoding = 'utf8') as out:
+    out.write(json.dumps(get_hierachy(),ensure_ascii=False,indent=4).encode('utf8').decode())
 
 with open('./out/chart.1.json', 'w',encoding = 'utf8') as out:
     out.write(json.dumps(split_chart(framed["hasTopConcept"][0],['ischart:Cretaceous','ischart:Paleogene','ischart:Neogene','ischart:Quaternary']),ensure_ascii=False,indent=4).encode('utf8').decode())
