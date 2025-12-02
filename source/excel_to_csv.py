e113863ec02dcbb4cec19420ce219ae8852e816f
Nicholas Car <nick@kurrawong.ai>
2025-12-02 15:26:08 +1000

    updated Excel to RDF

--- source/excel_to_csv.py
+++ source/excel_to_csv.py
@@ -3,13 +3,34 @@
 
 from pathlib import Path
 import pandas as pd
+from rdflib import Graph, URIRef, Literal, BNode, Namespace
+from rdflib.namespace import RDF, SDO, SKOS
 
+XKOS = Namespace("http://www.w3.org/2008/05/skos-xl#")
 
-xls = pd.ExcelFile(Path(__file__).parent / "chart-source.xlsx")
+namespaces = {
+    "gssp":     "http://resource.geosciml.org/ontology/gssp/",
+    "gts":      "http://resource.geosciml.org/ontology/timescale/gts#",
+    "ischart":  "http://resource.geosciml.org/classifier/ics/ischart/",
+    "strat":    "http://resource.geosciml.org/ontology/stratigraphy/",
+    "vis":      "http://resource.geosciml.org/ontology/ics-visual-chart/",
+}
+
+g = Graph().parse(Path(__file__).parent / "chart-multilang.ttl")
+
+xls = pd.ExcelFile(Path(__file__).parent / "chart-source-es-ES.xlsx")
 
 for sheet_name in xls.sheet_names:
-    if sheet_name not in ["README", "Purposes", "Template"]:
+    if sheet_name not in ["README", "default"]:
+        print(f"Sheet: {sheet_name}")
         df = pd.read_excel(xls, sheet_name=sheet_name)
         csv_file_name = f"{sheet_name}.csv"
-        df.to_csv(Path(__file__).parent / "csv" / csv_file_name, index=False)
-        print(f"Saved {sheet_name} to {csv_file_name}")
+
+        for i, row in df.iterrows():
+            parts = row.IRI.split(":")
+            iri = URIRef(namespaces[parts[0]] + parts[1])
+            assert iri in g.subjects(), f"IRI {iri} not in graph"
+            pl = Literal(row.Language, lang=sheet_name)
+            g.add((iri, SKOS.prefLabel, pl))
+
+print(g.serialize(destination="chart-multilang.ttl", format="longturtle"))
